207c66d4d8fe62f4ae5bdf068a30a88d7885ef6c
charlie <charlie@charliemakesthings.com>
2026-06-23 15:53:48 +0100

    updated build scripts

--- buildlibrary-dynamic.sh
+++ buildlibrary-dynamic.sh
@@ -1,5 +1,7 @@
 #! /bin/bash
 
+mkdir -p build/dylib
+
 gcc -c -fpic cheddaboards.c -o build/cheddaboards.o
 gcc -shared -o build/dylib/libcheddaboards.so build/cheddaboards.o
 #rm build/cheddaboards.o
--- buildlibrary-static.sh
+++ buildlibrary-static.sh
@@ -1,5 +1,7 @@
 #! /bin/bash
 
+mkdir -p build/static
+
 gcc -c cheddaboards.c -o build/cheddaboards.o
 ar rcs -o build/libcheddaboards.a build/cheddaboards.o
 mv build/libcheddaboards.a build/static/libcheddaboards.a
